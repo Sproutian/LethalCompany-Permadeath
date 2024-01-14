d35fd5a905e2aafca15dfaccf5a0aada58ccc958
jarme1 <156507648+Sproutian@users.noreply.github.com>
2024-01-14 16:50:07 -0600

    V1.0.1
    Fixed a softlock (when all players die, it now ends the game)

--- Patches/StartOfRoundPatch.cs
+++ Patches/StartOfRoundPatch.cs
@@ -22,18 +22,57 @@ namespace Permadeath.Patches
         {
             TimeOfDay timeOfDay = UnityEngine.Object.FindObjectOfType<TimeOfDay>();
             int daysToDeadline = timeOfDay.daysUntilDeadline;
-            if (daysToDeadline == 0 || (daysToDeadline == 1 && ___shipIsLeaving == true))
+            if (daysToDeadline == 0 || (daysToDeadline == 1 && ___shipIsLeaving == true) || (daysToDeadline == 3 && ___shipIsLeaving == false))
             {
+                FileLog.Log("Not reviving players (days til deadline: " + daysToDeadline + ")");
 
                 return true;
 
             }
 
-            //FileLog.Log("Reviving players (days til deadline: "+ daysToDeadline + ")");
+            FileLog.Log("Reviving players (days til deadline: "+ daysToDeadline + ")");
 
             return false;
         }
 
 
+        [HarmonyPatch("EndOfGame")]
+        [HarmonyPostfix]
+        static void endofgamePostfix(ref StartOfRound __instance, ref PlayerControllerB[] ___allPlayerScripts)
+        {
+
+
+            if (__instance.IsOwner)
+            {
+
+                bool runOver = true;
+                for (int i = 0; i < ___allPlayerScripts.Length; i++)
+                {
+                    if (!___allPlayerScripts[i].isPlayerDead && !___allPlayerScripts[i].isPlayerControlled)
+                    {
+                        continue;
+                    }
+                    else if (!___allPlayerScripts[i].isPlayerDead)
+                    {
+                        runOver = false;
+                    }
+                }
+                if (runOver)
+                {
+                    __instance.FirePlayersAfterDeadlineClientRpc(new int[4] { __instance.gameStats.daysSpent, __instance.gameStats.scrapValueCollected, __instance.gameStats.deaths, __instance.gameStats.allStepsTaken });
+                }
+            }
+        }
+
+
+        //[HarmonyPatch("ResetShip")]
+        //[HarmonyPostfix]
+        //static void resetShipPostfix(ref StartOfRound __instance)
+        //{
+        //    __instance.ReviveDeadPlayers();
+        //}
+
     }
+
+
 }
